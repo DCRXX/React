0f82033a2118df4d7ec1174b3d6b16eb554c74ff
DCRXX <linnik654g@gmail.com>
2026-02-22 22:56:20 +0300

    Beck

--- owl-top-beck/owl-top/src/main.ts
+++ owl-top-beck/owl-top/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // CORS
   app.enableCors({
-    origin: true,
+    origin: 'https://react-production-7245.up.railway.app',
     credentials: true,
   });
 
@@ -20,13 +20,12 @@ async function bootstrap() {
   
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  console.log(`📚 Swagger: http://localhost:3001/api`)
 
   // PORT
   const port = process.env.PORT ?? 3003;
   await app.listen(port);
 
-  console.log(`🚀 Server running on: http://localhost:${port}`);
+  console.log(`🚀 Server running on: https://react-production-7245.up.railway.app${port}`);
   console.log(`📚 Swagger UI: http://localhost:${port}/api`);
 }
 bootstrap();
